44bad2539328cef30102057e9acd07446a806abc
Giri Saurav <realanmup@gmail.com>
2021-02-17 12:08:03 +0545

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+         <link rel="stylesheet" type="text/css" href="style.css">
+         <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+        
+               <body>
+              <header>
+                  <section class="navsection">
+                      <div class ="logo">
+                          <h1>Web Development</h1>
+                        </div>
+                        <nav>
+                            <a href="#">Home</a>
+                            <a href="#">About us</a>
+                            <a href="#">Portfolio</a>
+                            <a href="#">Services</a>
+                            <a href="#">Contact</a>
+                        </nav>
+                        <main>
+                            <div class="leftside">
+                                <h3>Hello</h3>
+                                <h1>I'm Swikriti</h1>
+                                <h2>Intern and student</h2>
+                                <a href="#" class="btn1">Hire me</a>
+                                <a href="#" class="btn2">Contact me</a>
+                                
+
+                            </div>
+                            <div class="rightside">
+                                <img src="Images/intro.jpg">
+
+                            </div>
+                        </main>
+
+                  </section>
+              </header>
+        </body>
+    </head>
+</html>
